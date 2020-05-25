f3cbc1806e25d27aca5a36dded3cf2593b652be4
Zexal0807 <gallinar00@gmail.com>
2020-05-25 15:29:21 +0200

    Try to fix es2.2

--- lab 2/es2.s
+++ lab 2/es2.s
@@ -1,16 +1,52 @@
 .section .data
-	ma : .ascii " è maggiore di "
+	ma : .ascii "maggiore\n"
 	ma_l : .long - ma
 	
-	mi : .ascii " è minore di "
+	mi : .ascii "minore\n"
 	mi_l : .long - mi
 	
-	u : .ascii " è uguale a "
+	u : .ascii "uguale\n"
 	u_l : .long - u
 
 .section .text
 	.global _start
 _start:
-	
+
+	movl $4, %eax
+	movl $10, %ebx
+		
 	cmp %eax, %ebx
 	
+	je uguale	#op2 equal op1
+	jg minore	#op2 greater op1
+	
+maggiore:
+	movl $4, %eax
+	movl $1, %ebx
+	leal ma, %ecx
+	movl ma_l, %edx
+	int $0x80	
+
+	jmp fine
+	
+minore:
+	movl $4, %eax
+	movl $1, %ebx
+	leal mi, %ecx
+	movl mi_l, %edx
+	int $0x80
+
+	jmp fine
+	
+uguale:
+	movl $4, %eax
+	movl $1, %ebx
+	leal u, %ecx
+	movl u_l, %edx
+	int $0x80
+	
+fine:	
+	movl $1, %eax
+	movl $0, %ebx
+	int $0x80
+
